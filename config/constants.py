127ba7e5a24e5679a017edcf5e012c6b4f163854
tangtc <tangtc@gdshuyeit.com>
2023-07-26 22:56:26 +0800

    修改目录文件

---
+++ config/constants.py
@@ -0,0 +1,36 @@
+"""Constant variables."""
+
+from __future__ import annotations
+
+__all__=[
+    "SAFETY_PROMPT_FIELDS",
+    "DEFAULT_BOS_TOKEN",
+    "DEFAULT_EOS_TOKEN",
+    "PROMPT_USER",
+    "PROMPT_ASSISTANT",
+    "PROMPT_INPUT",
+    "PROMPT_DICT"
+]
+
+DEFAULT_BOS_TOKEN: str = "<s>"
+DEFAULT_EOS_TOKEN: str = "</s>"
+PROMPT_USER: str = "Human: {}"
+PROMPT_ASSISTANT: str ="\n\nAssistant: "
+PROMPT_INPUT=PROMPT_USER+PROMPT_ASSISTANT
+
+SAFETY_PROMPT_FIELDS: list[str] = [
+                    'Unfairness_And_Discrimination', 
+                    'Crimes_And_Illegal_Activities', 
+                    'Insult', 
+                    'Mental_Health',
+                    'Physical_Harm', 
+                    'Privacy_And_Property',
+                    'Ethics_And_Morality',
+                    "all"]
+
+PROMPT_DICT: dict[str,str] = {
+    "prompt_user": PROMPT_USER,
+    "prompt_assistant": PROMPT_ASSISTANT,
+    "prompt_input": PROMPT_INPUT
+}
+
